3ce6c0ccd7cbaf03cf6fb6a0a1cc69f8649acff1
Adrian Kaluzinski <kaluza@gmail.com>
2023-02-01 18:32:19 +0100

    Add test before implementation

--- src/test/java/eu/kaluzinski/credit_card/repositories/CreditCardRepositoryTest.java
+++ src/test/java/eu/kaluzinski/credit_card/repositories/CreditCardRepositoryTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CreditCardRepositoryTest {
 
-    final String CREDIT_CARD = "4744010848985281";
+    final String CREDIT_CARD_NUMBER = "4744010848985281";
 
     @Autowired
     private CreditCardRepository creditCardRepository;
@@ -23,16 +26,25 @@ class CreditCardRepositoryTest {
     @Autowired
     private EncryptionService encryptionService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void shouldSaveCreditCard() {
         CreditCard creditCard = new CreditCard();
-        creditCard.setCreditCardNumber(CREDIT_CARD);
+        creditCard.setCreditCardNumber(CREDIT_CARD_NUMBER);
         creditCard.setCvv("123");
         creditCard.setExpirationDate("12/25");
 
         CreditCard savedCreditCard = creditCardRepository.saveAndFlush(creditCard);
         CreditCard fetchedCreditCard = creditCardRepository.findById(savedCreditCard.getId()).get();
 
-        assertThat(fetchedCreditCard.getCreditCardNumber()).isEqualTo(CREDIT_CARD);
+        assertThat(getRows(savedCreditCard.getId())).isEqualTo(encryptionService.encrypt(CREDIT_CARD_NUMBER));
+        assertThat(fetchedCreditCard.getCreditCardNumber()).isEqualTo(CREDIT_CARD_NUMBER);
+    }
+
+    private String getRows(Long id) {
+        Map<String, Object> rows = jdbcTemplate.queryForMap("SELECT * FROM credit_card WHERE id="+id);
+        return (String) rows.get("credit_card_number");
     }
 }
